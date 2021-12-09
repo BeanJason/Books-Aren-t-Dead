4b033db107bd5bb99fa3d345aa28182b41f88d25
f-alshoh <alfaten22@yahoo.com>
2021-12-09 13:21:29 -0500

    Added dropdown and search bar to home page

--- index.php
+++ index.php
@@ -1,28 +1,49 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <title>Books Aren't Dead.com</title>
-        <meta charset="utf-8">
-        <link rel = "stylesheet" href="Style/main.css">
-        <script src="js/react.js"></script>
-        <script src="js/react-dom.js"></script>
-        <script src="js/navbar.js"></script>
-        <link rel="stylesheet" type="text/css" href="./Style/main.css" />
-        <link rel="stylesheet" type="text/css" href="./Style/nav-bar.css" />
-    </head>
-    <header>
-            <?php include "./templates/header.html" ?>
-         
-    </header>
-    <body>
-    <?php include "./templates/body.html"?>
 
-        <div id="logo"></div>
-        <script type = "text/javascript">
-            /* React code */
-            let h1 = React.createElement('h1',null, "Books Aren't Dead!");
-        </script>
-        
-        
-    </body>
+<head>
+    <title>Books Aren't Dead.com</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Style/main.css">
+    <script src="js/react.js"></script>
+    <script src="js/react-dom.js"></script>
+    <script src="js/navbar.js"></script>
+    <link rel="stylesheet" type="text/css" href="./Style/main.css" />
+    <link rel="stylesheet" type="text/css" href="./Style/nav-bar.css" />
+</head>
+<header>
+    <?php include "./templates/header.html" ?>
+
+</header>
+
+<body>
+    <?php include "./templates/body.html" ?>
+
+    <div id="logo"></div>
+    <script type="text/javascript">
+        /* React code */
+        let h1 = React.createElement('h1', null, "Books Aren't Dead!");
+    </script>
+
+    <br><br>
+
+    <div class="dropdown">
+        <button class="dropbtn">Books</button>
+        <div class="dropdown-content">
+            <a href="#">Arts & Photos</a>
+            <a href="#">Business</a>
+            <a href="#">Romance</a>
+            <a href="#">Childrens</a>
+            <a href="#">Law</a>
+            <a href="#">History</a>
+            <a href="#">Fiction</a>
+            <a href="#">Non-Fiction</a>
+            <a href="#">Thriller</a>
+        </div>
+    </div>
+
+    <input type="text" placeholder="Title, Author ,ISBN">
+    
+</body>
+
 </html>
