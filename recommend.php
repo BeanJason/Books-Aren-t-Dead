36a300bfe25ed422f7b4413789c13bad372dbca2
Emily <emcloke@umich.edu>
2021-12-12 13:52:10 -0500

    Recommendations Page

--- recommend.php
+++ recommend.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-</head>
-<body>
-         <?php include "./templates/header.php" ?>
-         <?php include "./templates/body.php"?>
-
-         <h1>Recommendations</h1>
-        <form action="updates.php" method="post">
-            <label for="Recommendation">Recommendation: </label>
-            <input type="text" id="Recommend" name="Recommend" placeholder="Add Recommendation"><br>
-            <input type="submit" value="submit">
-            <p> Posted on Date: <?php echo date ('Y');?> </p>
-        </form>
+  <head>
+  </head>
+  <body>
+    <?php include "./templates/header.php" ?>
+    <?php include "./templates/body.php"?>
+    <br>
+    <h1>Recommendations</h1>
+    <h3>2021 Top Picks</h3> <br>
+    <p>The Lyrics <br>
+    Call Us What We Carry <br>
+    The 1619 Project <br>
+    Under the Whispering Door <br>
+    The Sentence</p><br>
+  <?php 
+    $date = '';
+    $newDate = new DateTime($date);
+  ?>
+    <p>Date: <?php echo $newDate ->format ('jS F Y') ?> </p><br>
+    <h3>Holiday Favorites</h3> <br>
+    <p>A Christmas Carol <br>
+    How the Grinch Stole Christmas <br>
+    The Night Before Christmas <br>
+    Polar Express <br>
+    </p><br>
+    <p> Posted: 11th December 2021 </p>
     </body>
 </html>
